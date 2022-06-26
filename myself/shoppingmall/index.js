7e07aa368972c8abe8d24d4c99c8c69440b366c6
ddmkim94 <ddmkim94@gmail.com>
2022-06-26 14:02:42 +0900

    json 데이터 사용해서 상품 리스트 출력!

--- myself/shoppingmall/index.js
+++ myself/shoppingmall/index.js
@@ -1,2 +1,30 @@
-// strict 모드! => js의 비정상적인 문법을 엄격하게 잡아줌
+// strict mode => js의 비정상적인 문법을 엄격하게 잡아줌
 'use strict';
+
+let itemBox = document.querySelector('.item-box');
+let xhr = new XMLHttpRequest(); // XMLHttpRequest 객체 생성
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState == xhr.DONE && xhr.status == 200) {
+    let items = JSON.parse(xhr.responseText).items; // json to object
+    createItems(items);
+  }
+};
+xhr.open('GET', 'items.json', true);
+xhr.send();
+
+function createItems(items) {
+  items.forEach((item) => {
+    createItemHTML(item);
+  });
+}
+
+// html 생성
+function createItemHTML(item) {
+  let html = `
+    <div class="item">
+        <img src="${item.url}" class="icon" />
+        <span class="item-text">${item.gender} ${item.size} ${item.price}</span>
+    </div>`;
+  itemBox.innerHTML += html;
+}
