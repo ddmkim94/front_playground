b5d0252b2cadc1b8d99a3c5c06c393044c2d427e
ddmkim94 <ddmkim94@gmail.com>
2022-06-26 21:07:40 +0900

    목록 필터링 구현!

--- myself/shoppingmall/index.js
+++ myself/shoppingmall/index.js
@@ -1,23 +1,36 @@
-// strict mode => js의 비정상적인 문법을 엄격하게 잡아줌
 'use strict';
 
-let itemBox = document.querySelector('.item-box');
-let xhr = new XMLHttpRequest(); // XMLHttpRequest 객체 생성
+const itemBox = document.querySelector('.item-box');
+const btns = document.querySelector('.menu-box');
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState == xhr.DONE && xhr.status == 200) {
-    let items = JSON.parse(xhr.responseText).items; // json to object
-    createItems(items);
-  }
-};
-xhr.open('GET', 'items.json', true);
-xhr.send();
+let items;
 
-function createItems(items) {
+// fetch API로 바꿔보기..?
+const xhr = new XMLHttpRequest(); // XMLHttpRequest 객체 생성
+(function () {
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == xhr.DONE && xhr.status == 200) {
+      items = JSON.parse(xhr.responseText).items; // json to object
+      items.forEach((item) => createItemHTML(item));
+    }
+  };
+
+  // false는 코드가 비동기적으로 처리되지 않게 해주는 옵션 -> 비동기 좀 공부하자..
+  xhr.open('GET', 'items.json', false);
+  xhr.send();
+})();
+
+btns.addEventListener('click', (e) => {
+  if (e.target.tagName === 'DIV') return false;
+  let type = e.target.dataset.type;
+
+  itemBox.innerHTML = ''; // 기존 목록을 지우고 추가!
   items.forEach((item) => {
-    createItemHTML(item);
+    if (item.type === type || item.color === type) {
+      createItemHTML(item);
+    }
   });
-}
+});
 
 // html 생성
 function createItemHTML(item) {
